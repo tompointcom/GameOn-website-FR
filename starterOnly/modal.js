535a7dd5f5d0d945b4381b581b95139fbe586a8b
Tom Courcy <tom.courcy@gmail.com>
2024-06-20 14:49:32 +0200

    Issue #1 fermer la modale reglé + format d'image corrigé

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,13 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none"; 
+}
+
+// close modal event
+closeBtn.addEventListener("click", closeModal);
 
 
